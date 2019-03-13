cfb8c23f6fdff8e74257dae67c2d44a07bdc63c0
Mikhail <myakushev2@gmail.com>
2019-03-13 16:58:35 +0300

    new commit

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.practice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        goToNextActivity();
+        goToCatActivity();
+    }
+
+    public void goToNextActivity() {
+        Button button3 = findViewById(R.id.button3);
+        final Intent intent = new Intent(this, FoodChoose.class);
+        button3.setOnClickListener((v) -> startActivity(intent));
+    }
+
+    public void goToCatActivity() {
+        Button button2 = findViewById(R.id.button2);
+        final Intent intent = new Intent(this, Cat.class);
+        button2.setOnClickListener((v) -> startActivity(intent));
     }
 }
